230e193ef1192b4d04b31f69d42e3fa695c2d903
Romain Guillebert <romain.py@gmail.com>
2017-10-19 20:34:35 +0100

    Add the function starting the scrumblr container

--- run_scrumblr/docker.py
+++ run_scrumblr/docker.py
@@ -1,3 +1,5 @@
+import json
+
 import sh
 
 
@@ -7,6 +9,10 @@ def object_exists(object_id):
         return True
     except sh.ErrorReturnCode_1:
         return False
+
+
+def inspect(object_id):
+    return json.loads(str(sh.docker.inspect(object_id)))[0]
 
 
 def build_image(directory, tag):
@@ -20,9 +26,26 @@ def pull_image(image):
 
 
 def run_redis_for_container(container_name):
-    """Start the redis container for the (not yet running) container 'container_name'"""
+    """Start the redis container for the (not yet running) container
+    'container_name' and return the container_id"""
     pull_image("redis:latest")
 
     redis_container_name = container_name + "_redis"
     result = sh.docker.run("-d", "--name", redis_container_name, "redis:latest")
     return str(result).strip()
+
+
+def run_scrumblr_container(tag, container_name, redis_container_name):
+    """Start the scrumblr container specified by tag, link it with redis, and
+    return the port on which the host can reach the server"""
+    result = sh.docker.run(
+        "-d", "-P",
+        "--link", "{}:redis".format(redis_container_name),
+        "--name", container_name,
+        tag
+    )
+
+    container_id = str(result).strip()
+
+    container_info = inspect(container_id)
+    return container_info['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
--- run_scrumblr/tests/test_docker.py
+++ run_scrumblr/tests/test_docker.py
@@ -23,3 +23,18 @@ def test_run_redis_for_container(tmpdir):
     container_id = run_scrumblr.docker.run_redis_for_container(container_name)
 
     assert run_scrumblr.docker.object_exists(container_id)
+
+
+def test_run_scrumblr_container(tmpdir):
+    scrumblr_commit = "6efa17b790b33e9f5d2e8c88c360941ded5d6125"
+    scrumblr_tag = "scrumblr:" + scrumblr_commit
+    run_scrumblr.git.clone_and_checkout("https://github.com/rguillebert/scrumblr", tmpdir, scrumblr_commit)
+    run_scrumblr.docker.build_image(tmpdir, scrumblr_tag)
+
+    container_name = str(uuid.uuid4())
+
+    redis_container_name = run_scrumblr.docker.run_redis_for_container(container_name)
+
+    port = run_scrumblr.docker.run_scrumblr_container(scrumblr_tag, container_name, redis_container_name)
+
+    assert int(port)
