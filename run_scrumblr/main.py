321f643fa40acc17d825d27461e9469a8472f3fe
Romain Guillebert <romain.py@gmail.com>
2017-10-19 21:18:22 +0100

    Call the deploy code from main

--- run_scrumblr/main.py
+++ run_scrumblr/main.py
@@ -1,12 +1,14 @@
 import argparse
 
+from run_scrumblr.deploy import deploy_repository
+
 parser = argparse.ArgumentParser("Run different versions of Scrumblr in containers")
 parser.add_argument("repository", help="Url of the git repository")
 parser.add_argument("commit", help="Git commit of the version to be deployed")
 
 def main():
     args = parser.parse_args()
-    print args
+    deploy_repository(args.repository, args.commit)
 
 if __name__ == "__main__":
     main()
